21dd11ad80f86d4882dbceed214f4f8769963169
Konstantin Stepanyuk <k.stepanyuk@intabia.ru>
2020-06-21 08:12:51 +0700

    Добавлен пример разделения логики api и отрисковки html

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -1,86 +1,13 @@
 $(document).ready(function() {
-
     $("#get_all_files").click(function(){
-        $.ajax({
-            type : "GET",
-            url : "http://localhost:8080/photo-anthropology/files/all",
-            success: function(data){
-                for(let k in data) {
-                    $("#content").empty().append("<tr><th>id</th><th>File name</th><th>Date created</th></tr>" +
-                        "<tr><td>" + data[k].id + "</td>" +
-                        "<td>" + data[k].file_name + "</td>" +
-                        "<td>" + data[k].date_created["dayOfMonth"]+"."+data[k].date_created["month"]+"."+data[k].date_created["year"] + "</td>" +
-                        "<td><button>Удалить</button></td></tr>");
-                }
-            }
-        });
-    });
-
-    $("#get_all_groups").click(function(){
-        $.ajax({
-            type : "GET",
-            url : "http://localhost:8080/photo-anthropology/groups/all",
-            success: function(data){
-                for(let k in data) {
-                    $("#content").empty().append("" +
-                        "<tr><th>id</th><th>Group name</th><th>Group question</th></tr>" +
-                        "<tr><td>" + data[k].id + "</td>" +
-                        "<td>" + data[k].group_name + "</td>" +
-                        "<td>" + data[k].group_question+"</td>" +
-                        "<td><button>Удалить</button></td></tr>");
-                }
-            }
-        });
-    });
-
-    $("#get_group_by_id").click(function(){
-        $.ajax({
-            type : "GET",
-            url : "http://localhost:8080/photo-anthropology/groups/getGroup/id/",
-            data: $(id),
-            success: function(data){
-                $("#content").empty().append("<tr><th>id</th><th>Group name</th><th>Group question</th></tr><tr><td>" + data[k].id + "</td>" +
-                    "<td>" + data[k].group_name + "</td>" +
-                    "<td>" + data[k].group_question+"</td>" +
-                    "<td><button>Удалить</button></td></tr>");
-
-            }
-        });
-    });
-
-    $("#get_file_by_id").click(function(){
-        $.ajax({
-            type : "GET",
-            url : "http://localhost:8080/photo-anthropology/files/getFile/id/9",
-            success: function(data){
+        paApi.getAllFiles(function(fileList) {
+            for(let file in fileList) {
                 $("#content").empty().append("<tr><th>id</th><th>File name</th><th>Date created</th></tr>" +
-                    "<tr><td>" + data.id + "</td>" +
-                    "<td>" + data.file_name + "</td>" +
-                    "<td>" + data.date_created["dayOfMonth"]+"."+data.date_created["month"]+"."+data.date_created["year"] + "</td>" +
+                    "<tr><td>" + fileList[file].id + "</td>" +
+                    "<td>" + fileList[file].file_name + "</td>" +
+                    "<td>" + fileList[file].date_created["dayOfMonth"]+"."+fileList[file].date_created["month"]+"."+fileList[file].date_created["year"] + "</td>" +
                     "<td><button>Удалить</button></td></tr>");
             }
-        });
-    });
-
-    $(function() {
-        $('button[type=submit]').click(function(e) {
-            e.preventDefault();
-            $('input').next().remove();
-            $.ajax({
-                type: "POST",
-                url : 'groups/save',
-                data : $('form[name=groupForm]').serialize(),
-                success : function(res) {
-                    if(res.validated){
-                        $('#resultContainer pre code').text(JSON.stringify(res.groups));
-                        $('#resultContainer').show();
-                    }else{
-                        $.each(res.errorMessages,function(key,value){
-                            $('input[name='+key+']').after('<span class="error">'+value+'</span>');
-                        });
-                    }
-                }
-            })
         });
     });
 });
